8fd6066c2f7dc686339d36e98bdbec7882bce203
Paulo <paulo_esilva@yahoo.com.br>
2023-08-03 00:00:07 -0300

    imporatar aquivos e mostrar na tela

---
+++ Project-read-csv/Domain/Models/UploadCsv.cs
@@ -0,0 +1,48 @@
+﻿using Project_read_csv.Services;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace Project_read_csv.Domain.Models
+{
+    public class UploadCsv
+    {
+        public UploadCsv() { }
+        //id;tipocodigo;descricao;estoque;precovenda;precocusto;datahoracadastro
+        public UploadCsv(int id, int tipocodigo, string descricao, decimal estoque,
+            decimal precovenda, decimal precocusto, string datahoracadastro)
+        {
+            Id = id;
+            Tipocodigo = tipocodigo;
+            Descricao = descricao;
+            Estoque = estoque;
+            Precovenda = precovenda;
+            Precocusto = precocusto;
+            Datahoracadastro = datahoracadastro;
+        }
+
+        public int Id { get; set; }
+        public int Tipocodigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Estoque { get; set; }
+        public decimal Precovenda { get; set; }
+        public decimal Precocusto { get; set; }
+        public string Datahoracadastro { get; set; }
+
+        public static implicit operator string(UploadCsv uploadCsv) =>
+            $"{uploadCsv.Id},{uploadCsv.Tipocodigo},{uploadCsv.Descricao},{uploadCsv.Estoque}," +
+            $"{uploadCsv.Precovenda},{uploadCsv.Precocusto},{uploadCsv.Datahoracadastro}";
+
+        public static implicit operator UploadCsv(string line)
+        {
+            var data = line.Split(";");
+            return new UploadCsv(
+                Int32.Parse(data[0]),
+                Int32.Parse(data[1]),
+                data[2],
+                decimal.Parse(data[3]),
+                decimal.Parse(data[4]),
+                decimal.Parse(data[5]),
+                data[6]);
+        }
+    }
+}
---
+++ Project-read-csv/Services/UploadCsvService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Project_read_csv.Domain.Models;
+
+namespace Project_read_csv.Services
+{
+    public class UploadCsvService 
+    {
+        public async Task<List<UploadCsv>> GetCsvAsync(InputFileChangeEventArgs filecsv)
+        {
+            var file = filecsv.File;
+            string fileContent;
+            UploadCsv uploadCsvService;
+            var uploadCsvList = new List<UploadCsv>();
+
+            using (var stream = file.OpenReadStream())
+            using (var reader = new StreamReader(stream))
+            {
+                fileContent = await reader.ReadToEndAsync();
+            }
+            
+           var rows = fileContent.Split(new char[] { '\r','\n'});
+            try
+            {
+                foreach(var line in rows)
+                {
+                    if (line == rows[0] || line =="")
+                        continue;
+                    uploadCsvService = line;
+                    uploadCsvList.Add(uploadCsvService);
+                }
+            }
+            catch(Exception ex) { }
+
+            return uploadCsvList;
+        }
+    }
+}
